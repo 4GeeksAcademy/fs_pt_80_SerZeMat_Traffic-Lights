fd8e1372706c5804939fb00efa93b8d152300f53
SerZeMat <secemacorp@gmail.com>
2024-11-04 15:04:58 +0000

    Añadimos botón para incluir luz púrpura

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+	const [showPurple, setShowPurple] = useState(false); // Incializamos estado de la luz púrpura
+
+	const handleClick = (selectedColor) => {
+		setColor(selectedColor);
+	};
+
+	const addPurpleLight = () => {
+		setShowPurple((prev => !prev)); // Activamos y desactivamos la luz púrpura
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="traffic-light">
+				<div
+					className={`light red ${color === 'red' ? 'on' : ''}`}
+					onClick={() => handleClick('red')}
+				></div>
+				<div
+					className={`light yellow ${color === 'yellow' ? 'on' : ''}`}
+					onClick={() => handleClick('yellow')}
+				></div>
+				<div
+					className={`light green ${color === 'green' ? 'on' : ''}`}
+					onClick={() => handleClick('green')}
+				></div>
+
+				{showPurple && (
+					<div
+						className={`light purple ${color === 'purple' ? 'on' : ''}`} // Renderizamos la luz púrpura si el estado de showPurple es true
+						onClick={() => handleClick('purple')}
+					></div>
+				)}
+			</div>
+			<div className="botton">
+				<button onClick={addPurpleLight}>Add/Delete Purple Light</button>
+			</div>
 		</div>
 	);
 };
